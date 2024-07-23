2d46b227996c771b675a7ae078c460123d581d5b
Yue Wang <yuewang@Yues-MacBook-Pro.local>
2024-07-23 14:01:18 +0800

    modified values.yaml automatically

--- fix_misconfig.py
+++ fix_misconfig.py
@@ -1,10 +1,11 @@
 import yaml
+from ruamel.yaml import YAML
 
 def parse_opa_output(file_path):
     with open(file_path, 'r') as file:
         return file.read().strip('[]\n ').split(',')
 
-def fix_deployment(file_path):
+def fix_rendered_yaml(file_path):
     with open(file_path, 'r') as file:
         documents = yaml.safe_load_all(file)
         fixed_documents = []
@@ -20,12 +21,27 @@ def fix_deployment(file_path):
     with open(file_path, 'w') as file:
         yaml.safe_dump_all(fixed_documents, file)
 
+def fix_values_yaml(file_path):
+    yaml = YAML()
+    yaml.preserve_quotes = True
+
+    with open(file_path, 'r') as file:
+        values = yaml.load(file)
+        
+    if 'securityContext' not in values:
+        values['securityContext'] = {}
+    values['securityContext']['runAsNonRoot'] = True
+
+    with open(file_path, 'w') as file:
+        yaml.dump(values, file)
+
 def main():
     opa_output = parse_opa_output('misconfig_report.txt')
     for msg in opa_output:
         if "must not run as root" in msg:
-            fix_deployment('rendered.yaml')
-            print("Fixed: runAsNonRoot set to true in rendered.yaml")
+            fix_rendered_yaml('rendered.yaml')
+            fix_values_yaml('./webfrontendchart/values.yaml')
+            print("Fixed: runAsNonRoot set to true in rendered.yaml and values.yaml")
 
 if __name__ == "__main__":
     main()
